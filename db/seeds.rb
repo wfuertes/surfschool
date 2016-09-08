2d2b5bc7d6d4acafdd02041f53e88d0810f834ef
Willian F. Batista <wfuertes@gmail.com>
2016-09-08 15:38:24 -0300

    Adding stub users data; alter db to sqlite3; fixed create endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Rails.env.production?
+  connection = ActiveRecord::Base.connection
+  connection.tables.each do |table|
+    connection.execute("delete from #{table}; VACUUM;") unless table == "schema_migrations"
+  end
+
+  # Importing stub data
+  ["users"].each do |file_sql|
+    sql = File.read("db/stub/#{file_sql}.sql")
+    statements = sql.split(/;$/)
+    statements.pop
+
+    ActiveRecord::Base.transaction do
+      statements.each do |statement|
+        connection.execute(statement)
+      end
+    end
+  end
+end
