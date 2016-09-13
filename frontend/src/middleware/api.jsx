6257468beb84cedbe42f66ce8451d8c7405db58e
asaavedra <asaavedra@kenzan.com>
2016-09-13 11:37:01 -0300

    Cleaning comments

--- frontend/src/middleware/api.jsx
+++ frontend/src/middleware/api.jsx
@@ -4,22 +4,31 @@ import 'isomorphic-fetch';
 import { camelizeKeys } from 'humps';
 import { Schema, arrayOf, normalize } from 'normalizr';
 
-// Extracts the next page URL from Github API response.
-function getNextPageUrl(response) {
-  const link = response.headers.get('link');
-  if (!link) {
-    return null;
-  }
-
-  const nextLink = link.split(',').find(s => s.indexOf('rel="next"') > -1);
-  if (!nextLink) {
-    return null;
-  }
-
-  return nextLink.split(';')[0].slice(1, -1);
-}
+// Action key that carries API call info interpreted by this Redux middleware.
+export const CALL_API = Symbol('Call API');
 
 const API_ROOT = 'https://api.github.com/';
+
+// Read more about Normalizr: https://github.com/paularmstrong/normalizr
+const userSchema = new Schema('users', {
+  idAttribute: user => user.login.toLowerCase()
+});
+
+const repoSchema = new Schema('repos', {
+  idAttribute: repo => repo.fullName.toLowerCase()
+});
+
+repoSchema.define({
+  owner: userSchema
+});
+
+// Schemas for Github API responses.
+export const Schemas = {
+  USER: userSchema,
+  USER_ARRAY: arrayOf(userSchema),
+  REPO: repoSchema,
+  REPO_ARRAY: arrayOf(repoSchema)
+};
 
 // Fetches an API response and normalizes the result JSON according to schema.
 // This makes every API response have the same shape, regardless of how nested it was.
@@ -44,39 +53,21 @@ function callApi(endpoint, schema) {
     });
 }
 
-// We use this Normalizr schemas to transform API responses from a nested form
-// to a flat form where repos and users are placed in `entities`, and nested
-// JSON objects are replaced with their IDs. This is very convenient for
-// consumption by reducers, because we can easily build a normalized tree
-// and keep it updated as we fetch more data.
+// Extracts the next page URL from Github API response.
+function getNextPageUrl(response) {
+  const link = response.headers.get('link');
+  if (!link) {
+    return null;
+  }
 
-// Read more about Normalizr: https://github.com/paularmstrong/normalizr
+  const nextLink = link.split(',').find(s => s.indexOf('rel="next"') > -1);
+  if (!nextLink) {
+    return null;
+  }
 
-// GitHub's API may return results with uppercase letters while the query
-// doesn't contain any. For example, "someuser" could result in "SomeUser"
-// leading to a frozen UI as it wouldn't find "someuser" in the entities.
-// That's why we're forcing lower cases down there.
-const userSchema = new Schema('users', {
-  idAttribute: user => user.login.toLowerCase()
-});
+  return nextLink.split(';')[0].slice(1, -1);
+}
 
-const repoSchema = new Schema('repos', {
-  idAttribute: repo => repo.fullName.toLowerCase()
-});
-
-repoSchema.define({
-  owner: userSchema
-});
-
-// Schemas for Github API responses.
-export const Schemas = {
-  USER: userSchema,
-  USER_ARRAY: arrayOf(userSchema),
-  REPO: repoSchema,
-  REPO_ARRAY: arrayOf(repoSchema)
-};
-// Action key that carries API call info interpreted by this Redux middleware.
-export const CALL_API = Symbol('Call API');
 
 // A Redux middleware that interprets actions with CALL_API info specified.
 // Performs the call and promises when such actions are dispatched.
